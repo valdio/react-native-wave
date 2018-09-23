220fff39bea30f64cdd32bed60bfc15c17e37352
Valdio Veliu <valdioveliu@gmail.com>
2018-09-23 18:29:18 +0200

    working on the docs

--- Example/App.js
+++ Example/App.js
@@ -9,15 +9,15 @@ import Card from './app/components/Card'
 import {Wave, Animate, Composing, AnimationType} from 'react-native-wave'
 
 const AnimatedCard = Wave(Card)
-  .applyAnimation(Animate.opacity, 0, 1, 1000, AnimationType.timing, Easing.circle())
-  // .applyAnimation(Animate.width, 2, 400, 1000)
-  // .applyAnimation(Animate.padding, 0, 20, 2000, AnimationType.spring, Easing.elastic())
-  .applyAnimation(Animate.marginBottom, 0, 60, 2000)
-  .applyAnimation(Animate.padding, 0, 40, 2000)
-  .applyAnimationType(AnimationType.spring)
-  .applyEasing(Easing.bounce())
-  .applyDecayVelocity(0.07)
-  .applyComposition(Composing.parallel)
+// .applyAnimation(Animate.opacity, 0, 1, 1000, AnimationType.timing, Easing.circle())
+// .applyAnimation(Animate.width, 2, 400, 1000)
+// .applyAnimation(Animate.padding, 0, 20, 2000, AnimationType.spring, Easing.elastic())
+  .applyAnimation(Animate.marginBottom, 0, 60, 500, AnimationType.timing, Easing.elastic(5))
+  // .applyAnimation(Animate.padding, 0, 40, 2000)
+  // .applyAnimationType(AnimationType.spring)
+  // .applyEasing(Easing.bounce())
+  // .applyDecayVelocity(0.07)
+  // .applyComposition(Composing.parallel)
   .animate()
 
 export default class App extends Component {
